a892e722740ac7b2dd9cefc45500f459608df4f7
Stanislav Sandalnikov <s.sandalnikov@gmail.com>
2017-05-19 19:09:24 +0700

    gofmt

--- main.go
+++ main.go
@@ -1,73 +1,73 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-  "os"
-  "time"
-  "bytes"
-  "net/http"
-  "encoding/json"
+	"net/http"
+	"os"
+	"time"
 
-  "github.com/urfave/cli"
+	"github.com/urfave/cli"
 )
 
 var webhook string
 
 func toSlack(channelName string, message string) {
-  type payload struct {
-    Channel string `json:"channel"`
-    Text string `json:"text"`
-  }
+	type payload struct {
+		Channel string `json:"channel"`
+		Text    string `json:"text"`
+	}
 
-  jsonStr, err := json.Marshal(payload{ Channel: channelName, Text: message })
-  req, err := http.NewRequest("POST", webhook, bytes.NewBuffer(jsonStr))
-  if err != nil {
-      panic(err)
-  }
-  req.Header.Set("Content-Type", "application/json")
+	jsonStr, err := json.Marshal(payload{Channel: channelName, Text: message})
+	req, err := http.NewRequest("POST", webhook, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
 
-  client := &http.Client{}
-  resp, err := client.Do(req)
-  if err != nil {
-      panic(err)
-  }
-  defer resp.Body.Close() 
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 
-  timestamp := time.Now().Format(time.RFC1123)
-  fmt.Printf("%s: Message sent to channel %s\n", timestamp, channelName)
+	timestamp := time.Now().Format(time.RFC1123)
+	fmt.Printf("%s: Message sent to channel %s\n", timestamp, channelName)
 }
 
 func main() {
 	app := cli.NewApp()
-  app.Name = "slack-notifier"
-  app.Usage = "slack-notifier -c <channel_name> -m <message>"
-  app.Version = "0.0.1"
+	app.Name = "slack-notifier"
+	app.Usage = "slack-notifier -c <channel_name> -m <message>"
+	app.Version = "0.0.1"
 
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name: "channel, c",
-      Usage: "channel where to send the message",
-    },
-    cli.StringFlag{
-      Name: "message, m",
-      Usage: "message to send",
-    },
-  }
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "channel, c",
+			Usage: "channel where to send the message",
+		},
+		cli.StringFlag{
+			Name:  "message, m",
+			Usage: "message to send",
+		},
+	}
 
-  app.Action = func(c *cli.Context) error {
-    channelName := c.String("channel")
-    message := c.String("message")
+	app.Action = func(c *cli.Context) error {
+		channelName := c.String("channel")
+		message := c.String("message")
 
-    if !c.IsSet("channel") || !c.IsSet("message") {
-      fmt.Println("Usage:", app.Usage)
-      fmt.Println("\nRun \"slack-notifier help\" for more info")
-      return nil
-    }
+		if !c.IsSet("channel") || !c.IsSet("message") {
+			fmt.Println("Usage:", app.Usage)
+			fmt.Println("\nRun \"slack-notifier help\" for more info")
+			return nil
+		}
 
-    toSlack(channelName, message)
+		toSlack(channelName, message)
 
-    return nil
-  }
+		return nil
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
